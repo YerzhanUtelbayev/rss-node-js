8eeff3cc7576af2a7f84b10bb3c00ca7f9ace872
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-10-12 11:11:25 +0600

    feat: add crud api for user entity

---
+++ src/resources/users/user.db.js
@@ -0,0 +1,52 @@
+const mockUsers = require('./users.mock.json');
+const User = require('./user.model');
+
+class UserDB {
+  constructor(users) {
+    this._db = [...users];
+
+    this.create = this.create.bind(this);
+    this.findAll = this.findAll.bind(this);
+    this.findById = this.findById.bind(this);
+    this.updateOne = this.updateOne.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+
+  create(userData) {
+    if (this._db.some(({ name }) => name === userData.name)) {
+      throw new Error('User with this name already exists');
+    }
+    const user = new User(userData);
+    this._db = [...this._db, user];
+    return user;
+  }
+
+  findAll() {
+    return [...this._db];
+  }
+
+  findById(userId) {
+    return this._db.find(({ id }) => id === userId);
+  }
+
+  updateOne(userId, userData) {
+    this._db = this._db.map((user) => {
+      if (user.id === userId) {
+        return { ...user, ...userData };
+      }
+      return user;
+    });
+    return this._db.find(({ id }) => id === userId);
+  }
+
+  remove(userId) {
+    const user = this._db.find(({ id }) => id === userId);
+    if (!user) return null;
+    this._db = this._db.filter(({ id }) => id !== userId);
+    return user;
+  }
+}
+
+const usersDb = new UserDB(mockUsers.map((user) => new User(user)));
+
+module.exports = usersDb;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
 const User = require('./user.model');
-const usersService = require('./user.service');
+const userService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
+  const users = await userService.getAll();
   res.json(users.map(User.toResponse));
+});
+
+router.route('/').post(async (req, res) => {
+  const { body } = req;
+  const user = new User(body);
+
+  try {
+    const result = await userService.create(user);
+    if (!result) {
+      return res.sendStatus(400);
+    }
+    return res.json(User.toResponse(result));
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  const { id } = req.params;
+  const result = await userService.getById(id);
+  if (!result) {
+    return res.sendStatus(404);
+  }
+  return res.json(User.toResponse(result));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const { id } = req.params;
+  const { name, login, password } = req.body;
+
+  const result = await userService.update(id, { name, login, password });
+
+  if (!result) {
+    return res.sendStatus(400);
+  }
+
+  return res.json(User.toResponse(result));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const { id } = req.params;
+  const result = await userService.remove(id);
+  if (!result) {
+    return res.sendStatus(404);
+  }
+  return res.sendStatus(204);
 });
 
 module.exports = router;
