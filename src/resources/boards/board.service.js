13e590bc37623dd2b6b0966e060312887a086300
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-10-15 19:10:45 +0600

    refactor: transfer business-logic involving board tasks to service layer

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,4 +1,5 @@
 const boardRepo = require('./board.memory.repository');
+const taskService = require('../tasks/task.service');
 
 const create = (boardData) => boardRepo.create(boardData);
 
@@ -8,6 +9,13 @@ const getById = (boardId) => boardRepo.getById(boardId);
 
 const update = (boardId, boardData) => boardRepo.update(boardId, boardData);
 
-const remove = (boardId) => boardRepo.remove(boardId);
+const remove = async (boardId) => {
+  const tasks = await taskService.getByBoardId(boardId);
+  if (tasks.length > 0) {
+    tasks.forEach(({ id }) => taskService.remove(id));
+  }
+
+  return boardRepo.remove(boardId);
+};
 
 module.exports = { create, getAll, getById, update, remove };
